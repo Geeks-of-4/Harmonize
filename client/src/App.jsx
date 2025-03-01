d1357d3a3040bd1a5fea993cc1fe8d9166e0625b
cschiro <cschiro90@gmail.com>
2025-03-01 07:19:57 -0800

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,19 @@ import Artists from './components/Artists';
 import Intersect from './components/Intersect';
 import Map from './components/Map';
 import Nav from './components/Nav';
+const axios = require ('axios');
+
+const apiKey = "K2UGwYuaehCHov5Edy6YkJiYUlmKXPRB"
+//https://app.ticketmaster.com/discovery/v2/events.json?keyword=Kendrick+Lamar&startDateTime=2025-03-01T00:00:00Z&endDateTime=2025-08-31T00:00:00Z&apikey=K2UGwYuaehCHov5Edy6YkJiYUlmKXPRB
+
+const url = `https://app.ticketmaster.com/discovery/v2/events.json?keyword=Eagles&startDateTime=2025-03-01T00:00:00Z&endDateTime=2025-08-31T00:00:00Z&apikey=${apiKey}`
+
+const artist1='';
+const artist2='';
 
 function App() {
+
+
 
 
   return (
