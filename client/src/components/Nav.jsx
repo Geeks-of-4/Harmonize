4fd600ea1ed8f90ee400a337c1707119a5a90e9b
Brandon <bbeuttel@gmail.com>
2025-03-02 16:51:30 -0800

    nav bar v2

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import logo from '../assets/harmonizeLogo.png';
+import "./Nav.css";
+
+const Nav = ({ setDays, setMiles }) => {
+
+    return (
+
+     <div className='Nav'>
+      <img id='logo' src={logo}/>
+      <h2>Harmonize    Turn Every City into a Festival</h2>
+      <select id='days' onChange={(event) => setDays(event.target.value)}>
+        <option value='' disable selected>Select Days</option>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+        <option value='6'>6</option>
+        <option value='7'>7</option>
+      </select>
+      <select id='miles' onChange={(event) => setMiles(event.target.value)}>
+      <option value='' disabled selected>Select Distance</option>
+        <option value='10'>1</option>
+        <option value='25'>2</option>
+        <option value='50'>3</option>
+        <option value='100'>4</option>
+      </select>
+      <a id='about' href='http://google.com'>About</a>
+      <button className='navButtons'>Login</button>
+      <button className='navButtons'>SignUp</button>
+     </div>
+    )
+  }
+  
+  export default Nav
